e636e0e1bc243b32b48044c2d623b1cfb7d161a8
moseBunango <bunangomoses@gmail.com>
2023-04-15 10:31:27 +0300

    new branch commit

--- routes/web.php
+++ routes/web.php
@@ -32,5 +32,5 @@ Route::Resource('region','App\Http\Controllers\RegionController');
 Route::Resource('sub-region','App\Http\Controllers\Sub_RegionController');
 
 Route::Resource('bus-schedule','App\Http\Controllers\BusScheduleController');
-//Route::Resource('bus-schedule','App\Http\Controllers\BusScheduleController');
+
 
